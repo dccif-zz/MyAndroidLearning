55c1e3f5e1c0cbf6e3f63426db975e2fa6e292f4
dccif <heyujieboy@163.com>
2017-05-20 22:51:23 +0800

    FragementPractice-v1.1

--- FragmentBestPractice/app/src/main/java/com/example/com/fragmentbestpractice/NewsContentActivity.java
+++ FragmentBestPractice/app/src/main/java/com/example/com/fragmentbestpractice/NewsContentActivity.java
@@ -2,14 +2,14 @@ package com.example.com.fragmentbestpractice;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class NewsContentActivity extends AppCompatActivity {
 
     public static void actionStart(Context context,String newsTitle,String newsContent){
         Intent intent = new Intent(context,NewsContentActivity.class);
-        Intent.putExtra("new_title",newsTitle);
+        intent.putExtra("new_title", newsTitle);
         intent.putExtra("news_content",newsContent);
         context.startActivity(intent);
     }
@@ -20,8 +20,7 @@ public class NewsContentActivity extends AppCompatActivity {
         setContentView(R.layout.news_content);
         String newsTitle = getIntent().getStringExtra("news_title");
         String newsContent = getIntent().getStringExtra("news_content");
-        NewsContentFragment newsContentFragment = (NewsContentFragment);
-        getSupportFragmentManager().findFragmentById(R.id.news_content_fragment);
+        NewsContentFragment newsContentFragment = (NewsContentFragment) getSupportFragmentManager().findFragmentById(R.id.news_content_fragment);
         newsContentFragment.refresh(newsTitle,newsContent);
     }
 }
