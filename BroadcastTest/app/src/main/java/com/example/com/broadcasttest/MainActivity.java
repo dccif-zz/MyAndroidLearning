20b786b43a3f3293df74f5ac3c0593a63f70b4c5
dccif <heyujieboy@163.com>
2017-05-23 16:36:31 +0800

    Send broadcast

--- BroadcastTest/app/src/main/java/com/example/com/broadcasttest/MainActivity.java
+++ BroadcastTest/app/src/main/java/com/example/com/broadcasttest/MainActivity.java
@@ -9,6 +9,8 @@ import android.net.NetworkInfo;
 import android.os.NetworkOnMainThreadException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver = new NetworkChangeReceiver();
         registerReceiver(networkChangeReceiver,intentFilter);
+
+        Button button=(Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.broadcasttest.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
     }
 
     @Override
