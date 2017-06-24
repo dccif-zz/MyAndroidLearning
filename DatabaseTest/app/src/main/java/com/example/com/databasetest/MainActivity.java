df3f0a2be0a87dea53af40535880fab8c5c4efd8
dccif <heyujieboy@163.com>
2017-06-24 15:48:26 +0800

    Query

--- DatabaseTest/app/src/main/java/com/example/com/databasetest/MainActivity.java
+++ DatabaseTest/app/src/main/java/com/example/com/databasetest/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.com.databasetest;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -60,6 +62,31 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
                 db.delete("Book","pages > ?", new String[]{"500"});
+            }
+        });
+
+        Button queryButton = (Button) findViewById(R.id.query_data);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                //查询Book表中所有的数据
+                Cursor cursor = db.query("Book",null,null,null,null,null,null,null);
+                if (cursor.moveToFirst()) {
+                    do{
+                        //遍历Curose对象，取出数据并打印
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getInt(cursor.getColumnIndex("price"));
+
+                        Log.d("MainActivity","Book name is "+ name);
+                        Log.d("MainActivity","Book author is " + author);
+                        Log.d("MainActivity","Book pages is "+ pages);
+                        Log.d("MainActivity","Book price is "+ price);
+                    }while (cursor.moveToNext());
+                }
+                cursor.close();
             }
         });
 
