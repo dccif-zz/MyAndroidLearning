c35b8aa78e2e44a208cfd4cba7d51e60ef548c29
dccif <heyujieboy@163.com>
2017-09-29 19:45:18 +0800

    A Choose Class

--- CoolWeather/app/src/main/java/com/coolweather/android/db/County.java
+++ CoolWeather/app/src/main/java/com/coolweather/android/db/County.java
@@ -6,10 +6,10 @@ import org.litepal.crud.DataSupport;
  * Created by heyujie on 2017/9/29.
  */
 
-public class Country extends DataSupport {
+public class County extends DataSupport {
     private int id;
 
-    private String countryName;
+    private String countyName;
 
     private String weatherId;
 
@@ -23,12 +23,20 @@ public class Country extends DataSupport {
         this.id = id;
     }
 
-    public String getCountryName() {
-        return countryName;
+    public String getCountyName() {
+        return countyName;
     }
 
-    public void setCountryName(String countryName) {
-        this.countryName = countryName;
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getcountyName() {
+        return countyName;
+    }
+
+    public void setcountyName(String countyName) {
+        this.countyName = countyName;
     }
 
     public String getWeatherId() {
